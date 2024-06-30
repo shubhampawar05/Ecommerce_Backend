180cff4768ab901524319761f339004064d63d24
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-30 13:47:01 +0530

    cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
